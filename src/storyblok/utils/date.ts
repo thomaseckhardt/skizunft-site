f9d391537a0c4110ba1facc693850f9c06b23ef0
Thomas Eckhardt <mail@thomaseckhardt.com>
2023-10-23 13:42:42 +0200

    stuff

--- src/storyblok/utils/date.ts
+++ src/storyblok/utils/date.ts
@@ -1,6 +1,18 @@
-import type { SbBlokData } from '@storyblok/astro'
-import { storyblokEditable } from '@storyblok/astro'
+export function parseDate(date) {
+  if (!date) return
+  return new Date(storyblokDate(date))
+}
 
 export function storyblokDate(date) {
+  if (!date) return
   return `${date}+0000`
+}
+
+export function getQueryDate(date) {
+  if (!date) return
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  const formattedDate = `${year}-${month}-${day}`
+  return formattedDate
 }
---
+++ src/storyblok/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './assets'
+export * from './datasources'
+export * from './date'
+export * from './fetchStories'
+export * from './richtext'
