ddae01af39ee50a8bad83af19f5afacc9a143722
Thomas Eckhardt <mail@thomaseckhardt.com>
2024-10-25 23:47:50 +0200

    Test new function

--- netlify/functions/send-booking-confirmation.ts
+++ netlify/functions/send-booking-confirmation.ts
@@ -1,14 +1,12 @@
-import type { Handler } from '@netlify/functions'
-
-import { sendEmail } from "@netlify/emails";
+import type { Handler } from '@netlify/functions';
 
 const handler: Handler = async function (event) {
-  console.log('send-booking-confirmation handler')
+  console.log('send-booking-confirmation handler');
   if (event.body === null) {
     return {
       statusCode: 400,
-      body: JSON.stringify('Payload required'),
-    }
+      body: JSON.stringify('Payload required')
+    };
   }
 
   // if (process.env.BOOKING_EMAIL_FROM) {
@@ -19,37 +17,46 @@ const handler: Handler = async function (event) {
   // }
 
   // Rename to requestBody
-  const data = JSON.parse(event.body)
+  const data = JSON.parse(event.body);
 
   if (!data.email) {
     return {
       statusCode: 400,
-      body: JSON.stringify('Recipient email required'),
-    }
+      body: JSON.stringify('Recipient email required')
+    };
   }
 
-  console.log('send mail ')
+  console.log('send mail to ', data.email);
 
-    try {
-      const result = await sendEmail({
-      from: 'buchung@szkollnau.de',
-      to: data.email,
-      subject: `Deine Buchung ist bestätigt 🥳 #${data.orderNumber}`,
-      template: "booking-confirmation",
-      parameters: JSON.parse(event.body),
-    });
+  try {
+    const result = await fetch(
+      `${process.env.URL}/.netlify/functions/emails/booking-confirmation`,
+      {
+        headers: {
+          'netlify-emails-secret': process.env.NETLIFY_EMAILS_SECRET
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          from: 'buchung@szkollnau.de',
+          to: data.email,
+          subject: `Deine Buchung ist bestätigt 🥳 #${data.orderNumber}`,
+          template: 'booking-confirmation',
+          parameters: JSON.parse(event.body),
+          MessageStream: 'booking-confirmation'
+        })
+      }
+    );
 
-
-    console.log('result', result)
+    console.log('result', result);
 
     return {
       statusCode: 200,
-      body: JSON.stringify('Booking confirmation email sent!'),
-    }
+      body: JSON.stringify('Booking confirmation email sent!')
+    };
   } catch (error) {
-    console.log('error', error)
-    return error
+    console.log('error', error);
+    return error;
   }
-}
+};
 
-export { handler }
+export { handler };
